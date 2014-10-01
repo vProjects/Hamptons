f1ff1cb205061545c8b9ca460ca227f1730f13e4
vProjects <github@vyrazu.com>
2014-10-01 16:16:24 +0530

    Initial Commit

---
+++ js/coustom-modal.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+	$('#javaClick').click(function(){
+		$('#javaText').css({"color":"red","font-size":"28px"});
+	});
+	
+	$('#myImage').click(function() {
+ 	 	var f = $(this).attr('src');
+ 	});
+ 	
+ 	$('.modal-content').click(function() {
+ 	 	var f;
+ 	});
+});
---
+++ js/custom.js
@@ -0,0 +1,125 @@
+/**
+ * @author Thakur
+ */
+
+$(document).ready(function(){
+	
+	// options for slider carousel
+	var active_img = "0";
+	//getting active image of jessore slider
+	$('#img_picker').click(function(){
+		var a = $('.img_active');
+		for(var i=0; i<a.length; i++)
+		{
+			//checking that left is 0
+			if(a[i].style.left == '0px')
+			{
+				var index = i;
+			}
+		}
+		active_img = $('.img_active')[index].children[0].src;
+		
+		$('#large_image').attr('src',active_img);
+		$('#myModal').modal('show');
+	});
+	
+	$('#map_open').click(function(){
+		setTimeout(function(){
+            //$('#map_container').append($("#map_canvas").css("margin-top","0px").get(0));
+        },500);
+		$('#myMap').modal('show');
+	});
+	
+	//activating nav-tabs
+	
+	$(function () {
+	  $('#myTab a:first').tab('show');
+	});
+	
+	$(function () {
+	  $('#propertyView a:first').tab('show');
+	});
+	
+	//check all checkboxes 
+	 $(document).on('click', '#east-hampton-area', function(){
+	  if(this.checked == true){
+	   $('.chk-bx-all').prop('checked', true);
+	  }
+	  else
+	  {
+	   $('.chk-bx-all').prop('checked', false);
+	  }
+	 });
+	 
+	 $(document).on('click', '#bridge-hampton-area', function(){
+	  if(this.checked == true){
+	   $('.chk-bx-bridge').prop('checked', true);
+	  }
+	  else
+	  {
+	   $('.chk-bx-bridge').prop('checked', false);
+	  }
+	 });
+	 
+	  $(document).on('click', '#sag-harbor-area', function(){
+	  if(this.checked == true){
+	   $('.chk-bx-sag').prop('checked', true);
+	  }
+	  else
+	  {
+	   $('.chk-bx-sag').prop('checked', false);
+	  }
+	 });
+	 
+	  $(document).on('click', '#southampton-area', function(){
+	  if(this.checked == true){
+	   $('.chk-bx-south').prop('checked', true);
+	  }
+	  else
+	  {
+	   $('.chk-bx-south').prop('checked', false);
+	  }
+	 });
+	 
+	  $(document).on('click', '#westhampton-area', function(){
+	  if(this.checked == true){
+	   $('.chk-bx-west').prop('checked', true);
+	  }
+	  else
+	  {
+	   $('.chk-bx-west').prop('checked', false);
+	  }
+	 });
+	 
+	  $(document).on('click', '#north-fork', function(){
+	  if(this.checked == true){
+	   $('.chk-bx-north').prop('checked', true);
+	  }
+	  else
+	  {
+	   $('.chk-bx-north').prop('checked', false);
+	  }
+	 });
+	 
+	 // change content of town area collapsible on click
+		$('.SeeMore2').click(function(){
+			var $this = $(this);
+			$this.toggleClass('SeeMore2');
+			if($this.hasClass('SeeMore2')){
+				$this.text('+');			
+			} else {
+				$this.text('-');
+			}
+		});
+		
+		
+		//same  height coloumns
+		
+		  if ($(window).width() >= 768) {
+			  $('.right-block').css({
+			  	height: $("#first-col").height() - $("#rt-nav-dash").height() -10
+			 });
+		  }
+		          
+		
+});
